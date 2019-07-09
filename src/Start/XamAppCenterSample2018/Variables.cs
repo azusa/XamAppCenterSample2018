0520903887b34f0f0f13ffe530d0734eb9ce4c01
Hiroyuki Ohnaka <h.onaka@gxp.co.jp>
2019-07-09 22:27:37 +0900

    Edit for iOS

--- src/Start/XamAppCenterSample2018/Variables.cs
+++ src/Start/XamAppCenterSample2018/Variables.cs
@@ -5,6 +5,6 @@ namespace XamAppCenterSample2018
     public static class Variables
     {
         // NOTE: Replace this example key with a valid subscription key.
-        public static readonly string ApiKey = "PLEASE ENTER YOUR KEY";
+        public static readonly string ApiKey = "29baf92bbb974d5abb3ec431a4f89792";
     }
 }
