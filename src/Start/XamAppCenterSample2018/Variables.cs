5028b39738ab104973159c1e3b322139bbe0ea91
Hiroyuki Ohnaka <h.onaka@gxp.co.jp>
2019-07-09 23:24:29 +0900

    バンドル識別子を修正

--- src/Start/XamAppCenterSample2018/Variables.cs
+++ src/Start/XamAppCenterSample2018/Variables.cs
@@ -5,6 +5,6 @@ namespace XamAppCenterSample2018
     public static class Variables
     {
         // NOTE: Replace this example key with a valid subscription key.
-        public static readonly string ApiKey = "29baf92bbb974d5abb3ec431a4f89792";
+        public static readonly string ApiKey = "[ENTER YOUR API KEY]";
     }
 }
--- src/Start/iOS/Views/MainView.cs
+++ src/Start/iOS/Views/MainView.cs
@@ -1,166 +1,166 @@
-﻿using System;
-using System;
-using UIKit;
-using Foundation;
-using CoreGraphics;
-using MvvmCross.Binding.BindingContext;
-using MvvmCross.Platforms.Ios.Presenters.Attributes;
-using MvvmCross.Platforms.Ios.Views;
-using XamAppCenterSample2018.ViewModels;
+﻿    using System;
+    using System;
+    using UIKit;
+    using Foundation;
+    using CoreGraphics;
+    using MvvmCross.Binding.BindingContext;
+    using MvvmCross.Platforms.Ios.Presenters.Attributes;
+    using MvvmCross.Platforms.Ios.Views;
+    using XamAppCenterSample2018.ViewModels;
 
-namespace XamAppCenterSample2018.iOS.Views
-{
-
-    [Register("MainView")]
-    [MvxRootPresentation(WrapInNavigationController = false)]
-    public class MainView : MvxViewController<MainViewModel>
+    namespace XamAppCenterSample2018.iOS.Views
     {
-        static readonly nfloat fontSize = 20;
 
-        UILabel inputLabel;
-        UITextView inputText;
-        UIButton translateButton;
-        UILabel translatedLabel;
-        UITextView translatedText;
-
-        void InitUI()
+        [Register("MainView")]
+        [MvxRootPresentation(WrapInNavigationController = false)]
+        public class MainView : MvxViewController<MainViewModel>
         {
-            View.ContentMode = UIViewContentMode.ScaleToFill;
-            View.LayoutMargins = new UIEdgeInsets(0, 16, 0, 16);
-            View.Frame = new CGRect(0, 0, 375, 667);
-            View.BackgroundColor = UIColor.White;
-            View.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+            static readonly nfloat fontSize = 20;
 
-            inputLabel = new UILabel
+            UILabel inputLabel;
+            UITextView inputText;
+            UIButton translateButton;
+            UILabel translatedLabel;
+            UITextView translatedText;
+
+            void InitUI()
             {
-                Frame = new CGRect(0, 0, 375, 20),
-                Opaque = false,
-                UserInteractionEnabled = false,
-                ContentMode = UIViewContentMode.Left,
-                Text = "翻訳したい日本語",
-                TextAlignment = UITextAlignment.Left,
-                LineBreakMode = UILineBreakMode.TailTruncation,
-                Lines = 0,
-                BaselineAdjustment = UIBaselineAdjustment.AlignBaselines,
-                AdjustsFontSizeToFitWidth = false,
-                TranslatesAutoresizingMaskIntoConstraints = false,
-                Font = UIFont.SystemFontOfSize(fontSize),
-            };
-            View.AddSubview(inputLabel);
+                View.ContentMode = UIViewContentMode.ScaleToFill;
+                View.LayoutMargins = new UIEdgeInsets(0, 16, 0, 16);
+                View.Frame = new CGRect(0, 0, 375, 667);
+                View.BackgroundColor = UIColor.White;
+                View.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
 
-            inputLabel.HeightAnchor.ConstraintEqualTo(20).Active = true;
-            inputLabel.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+                inputLabel = new UILabel
+                {
+                    Frame = new CGRect(0, 0, 375, 20),
+                    Opaque = false,
+                    UserInteractionEnabled = false,
+                    ContentMode = UIViewContentMode.Left,
+                    Text = "翻訳したい日本語",
+                    TextAlignment = UITextAlignment.Left,
+                    LineBreakMode = UILineBreakMode.TailTruncation,
+                    Lines = 0,
+                    BaselineAdjustment = UIBaselineAdjustment.AlignBaselines,
+                    AdjustsFontSizeToFitWidth = false,
+                    TranslatesAutoresizingMaskIntoConstraints = false,
+                    Font = UIFont.SystemFontOfSize(fontSize),
+                };
+                View.AddSubview(inputLabel);
 
-            inputLabel.TopAnchor.ConstraintEqualTo(View.TopAnchor, 70).Active = true;
-            inputLabel.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
-            inputLabel.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
+                inputLabel.HeightAnchor.ConstraintEqualTo(20).Active = true;
+                inputLabel.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
 
-            inputText = new UITextView
-            {
-                Frame = new CGRect(0, 0, 375, 200),
-                ContentMode = UIViewContentMode.ScaleToFill,
-                TranslatesAutoresizingMaskIntoConstraints = false,
-                KeyboardType = UIKeyboardType.Twitter,
-                Font = UIFont.SystemFontOfSize(fontSize),
-                AccessibilityIdentifier = "inputText",
-            };
+                inputLabel.TopAnchor.ConstraintEqualTo(View.TopAnchor, 70).Active = true;
+                inputLabel.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
+                inputLabel.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
 
-            inputText.Layer.BorderWidth = 1;
-            inputText.Layer.BorderColor = UIColor.LightGray.CGColor;
+                inputText = new UITextView
+                {
+                    Frame = new CGRect(0, 0, 375, 200),
+                    ContentMode = UIViewContentMode.ScaleToFill,
+                    TranslatesAutoresizingMaskIntoConstraints = false,
+                    KeyboardType = UIKeyboardType.Twitter,
+                    Font = UIFont.SystemFontOfSize(fontSize),
+                    AccessibilityIdentifier = "inputText",
+                };
 
-            View.AddSubview(inputText);
+                inputText.Layer.BorderWidth = 1;
+                inputText.Layer.BorderColor = UIColor.LightGray.CGColor;
 
-            inputText.HeightAnchor.ConstraintEqualTo(View.HeightAnchor, 0.3f).Active = true;
-            inputText.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+                View.AddSubview(inputText);
 
-            inputText.TopAnchor.ConstraintEqualTo(inputLabel.BottomAnchor, 5).Active = true;
-            inputText.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
-            inputText.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
+                inputText.HeightAnchor.ConstraintEqualTo(View.HeightAnchor, 0.3f).Active = true;
+                inputText.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
 
-            var toolBar = new UIToolbar
-            {
-                BarStyle = UIBarStyle.Default,
-                TranslatesAutoresizingMaskIntoConstraints = false,
-            };
-            toolBar.HeightAnchor.ConstraintEqualTo(40).Active = true;
-            toolBar.WidthAnchor.ConstraintEqualTo(View.Frame.Width).Active = true;
+                inputText.TopAnchor.ConstraintEqualTo(inputLabel.BottomAnchor, 5).Active = true;
+                inputText.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
+                inputText.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
 
-            var spacer = new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace);
-            var commitButton = new UIBarButtonItem(UIBarButtonSystemItem.Done);
+                var toolBar = new UIToolbar
+                {
+                    BarStyle = UIBarStyle.Default,
+                    TranslatesAutoresizingMaskIntoConstraints = false,
+                };
+                toolBar.HeightAnchor.ConstraintEqualTo(40).Active = true;
+                toolBar.WidthAnchor.ConstraintEqualTo(View.Frame.Width).Active = true;
 
-            commitButton.Clicked += (s, e) => View.EndEditing(true);
-            toolBar.SetItems(new UIBarButtonItem[] { spacer, commitButton }, false);
-            inputText.InputAccessoryView = toolBar;
+                var spacer = new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace);
+                var commitButton = new UIBarButtonItem(UIBarButtonSystemItem.Done);
 
-            translateButton = new UIButton(UIButtonType.RoundedRect)
-            {
-                Frame = new CGRect(0, 0, 375, 20),
-                Opaque = false,
-                ContentMode = UIViewContentMode.ScaleToFill,
-                HorizontalAlignment = UIControlContentHorizontalAlignment.Center,
-                VerticalAlignment = UIControlContentVerticalAlignment.Center,
-                LineBreakMode = UILineBreakMode.MiddleTruncation,
-                TranslatesAutoresizingMaskIntoConstraints = false,
-                Font = UIFont.SystemFontOfSize(fontSize),
-                AccessibilityIdentifier = "translateButton",
-            };
+                commitButton.Clicked += (s, e) => View.EndEditing(true);
+                toolBar.SetItems(new UIBarButtonItem[] { spacer, commitButton }, false);
+                inputText.InputAccessoryView = toolBar;
 
-            translateButton.SetTitle("英語に翻訳する", UIControlState.Normal);
-            View.AddSubview(translateButton);
+                translateButton = new UIButton(UIButtonType.RoundedRect)
+                {
+                    Frame = new CGRect(0, 0, 375, 20),
+                    Opaque = false,
+                    ContentMode = UIViewContentMode.ScaleToFill,
+                    HorizontalAlignment = UIControlContentHorizontalAlignment.Center,
+                    VerticalAlignment = UIControlContentVerticalAlignment.Center,
+                    LineBreakMode = UILineBreakMode.MiddleTruncation,
+                    TranslatesAutoresizingMaskIntoConstraints = false,
+                    Font = UIFont.SystemFontOfSize(fontSize),
+                    AccessibilityIdentifier = "translateButton",
+                };
 
-            translateButton.HeightAnchor.ConstraintEqualTo(40f).Active = true;
-            translateButton.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+                translateButton.SetTitle("英語に翻訳する", UIControlState.Normal);
+                View.AddSubview(translateButton);
 
-            translateButton.TopAnchor.ConstraintEqualTo(inputText.BottomAnchor, 20).Active = true;
-            translateButton.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
-            translateButton.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
+                translateButton.HeightAnchor.ConstraintEqualTo(40f).Active = true;
+                translateButton.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
 
-            translatedLabel = new UILabel
-            {
-                Frame = new CGRect(0, 0, 375, 20),
-                Opaque = false,
-                UserInteractionEnabled = false,
-                ContentMode = UIViewContentMode.Left,
-                Text = "翻訳された英語",
-                TextAlignment = UITextAlignment.Left,
-                LineBreakMode = UILineBreakMode.TailTruncation,
-                Lines = 0,
-                BaselineAdjustment = UIBaselineAdjustment.AlignBaselines,
-                AdjustsFontSizeToFitWidth = false,
-                TranslatesAutoresizingMaskIntoConstraints = false,
-                Font = UIFont.SystemFontOfSize(fontSize),
-            };
-            View.AddSubview(translatedLabel);
+                translateButton.TopAnchor.ConstraintEqualTo(inputText.BottomAnchor, 20).Active = true;
+                translateButton.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
+                translateButton.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
 
-            translatedLabel.HeightAnchor.ConstraintEqualTo(20).Active = true;
-            translatedLabel.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+                translatedLabel = new UILabel
+                {
+                    Frame = new CGRect(0, 0, 375, 20),
+                    Opaque = false,
+                    UserInteractionEnabled = false,
+                    ContentMode = UIViewContentMode.Left,
+                    Text = "翻訳された英語",
+                    TextAlignment = UITextAlignment.Left,
+                    LineBreakMode = UILineBreakMode.TailTruncation,
+                    Lines = 0,
+                    BaselineAdjustment = UIBaselineAdjustment.AlignBaselines,
+                    AdjustsFontSizeToFitWidth = false,
+                    TranslatesAutoresizingMaskIntoConstraints = false,
+                    Font = UIFont.SystemFontOfSize(fontSize),
+                };
+                View.AddSubview(translatedLabel);
 
-            translatedLabel.TopAnchor.ConstraintEqualTo(translateButton.BottomAnchor, 20).Active = true;
-            translatedLabel.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
-            translatedLabel.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
+                translatedLabel.HeightAnchor.ConstraintEqualTo(20).Active = true;
+                translatedLabel.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
 
-            translatedText = new UITextView
-            {
-                Frame = new CGRect(0, 0, 375, 200),
-                ContentMode = UIViewContentMode.ScaleToFill,
-                TranslatesAutoresizingMaskIntoConstraints = false,
-                Font = UIFont.SystemFontOfSize(fontSize),
-                AccessibilityIdentifier = "translatedText",
-                Editable = false,
-            };
+                translatedLabel.TopAnchor.ConstraintEqualTo(translateButton.BottomAnchor, 20).Active = true;
+                translatedLabel.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
+                translatedLabel.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
 
-            translatedText.Layer.BorderWidth = 1;
-            translatedText.Layer.BorderColor = UIColor.LightGray.CGColor;
+                translatedText = new UITextView
+                {
+                    Frame = new CGRect(0, 0, 375, 200),
+                    ContentMode = UIViewContentMode.ScaleToFill,
+                    TranslatesAutoresizingMaskIntoConstraints = false,
+                    Font = UIFont.SystemFontOfSize(fontSize),
+                    AccessibilityIdentifier = "translatedText",
+                    Editable = false,
+                };
 
-            View.AddSubview(translatedText);
+                translatedText.Layer.BorderWidth = 1;
+                translatedText.Layer.BorderColor = UIColor.LightGray.CGColor;
 
-            translatedText.HeightAnchor.ConstraintEqualTo(View.HeightAnchor, 0.3f).Active = true;
-            translatedText.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+                View.AddSubview(translatedText);
 
-            translatedText.TopAnchor.ConstraintEqualTo(translatedLabel.BottomAnchor, 5).Active = true;
-            translatedText.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
-            translatedText.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
-        }
+                translatedText.HeightAnchor.ConstraintEqualTo(View.HeightAnchor, 0.3f).Active = true;
+                translatedText.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+
+                translatedText.TopAnchor.ConstraintEqualTo(translatedLabel.BottomAnchor, 5).Active = true;
+                translatedText.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
+                translatedText.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
+            }
 
             void SetBinding()
             {
@@ -173,15 +173,15 @@ namespace XamAppCenterSample2018.iOS.Views
                 set.Apply();
             }
 
-                    public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
+            public override void ViewDidLoad()
+            {
+                base.ViewDidLoad();
 
-            InitUI();
-            SetBinding();
+                InitUI();
+                SetBinding();
+            }
         }
+
+
+
     }
-
-    
-
-}
