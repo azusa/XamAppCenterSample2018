309d17746ee0fc4c2db8a12694b04e4a52d34b44
Hiroyuki Onaka <h.onaka@gxp.co.jp>
2019-07-25 17:53:13 +0900

    NUnit.FramrworkをDroidプロジェクトにいれる

---
+++ src/Start/Droid/appcenter-post-build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Post Build Script
+
+# Exit immediately if a command exits with a non-zero status (failure)
+set -e 
+
+##################################################
+# variables
+
+appCenterLoginApiToken=$AppCenterLoginToken # this comes from the build environment variables
+appName="TomohiroSuzuki128/XamAppCenterSample2018Droid"
+deviceSetName="TomohiroSuzuki128/my-devices-android"
+publishedAppFileName="com.hiro128777.XamAppCenterSample2018.apk"
+sourceFileRootDir="$APPCENTER_SOURCE_DIRECTORY/src/Finish"
+uiTestProjectName="UITests"
+testSeriesName="all-tests-android"
+##################################################
+
+echo "##################################################################################################"
+echo "Post Build Script"
+echo "##################################################################################################"
+echo "Starting Xamarin.UITest"
+echo "   App Name: $appName"
+echo " Device Set: $deviceSetName"
+echo "Test Series: $testSeriesName"
+echo "##################################################################################################"
+echo ""
+
+echo "> Build UI test projects"
+find $sourceFileRootDir -regex '.*Test.*\.csproj' -exec msbuild {} \;
+
+echo "> Run UI test command"
+# Note: must put a space after each parameter/value pair
+appcenter test run uitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/$publishedAppFileName --test-series $testSeriesName --locale "ja_JP" --build-dir $sourceFileRootDir/$uiTestProjectName/bin/Debug --uitest-tools-dir $sourceFileRootDir/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken 
+
+echo ""
+echo "##################################################################################################"
+echo "Post Build Script complete"
+echo "##################################################################################################"
